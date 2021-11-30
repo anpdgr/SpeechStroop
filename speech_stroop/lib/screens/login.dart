5f477033335cb96e80cfa0b27e1535e1af994788
anpdgr <ant-dgr@hotmail.com>
2021-12-01 00:42:07 +0700

    validate input and login as guest button

--- speech_stroop/lib/screens/login.dart
+++ speech_stroop/lib/screens/login.dart
@@ -15,6 +15,7 @@ class _LoginWidgetWidgetState extends State<LoginWidget> {
   TextEditingController passwordController;
   bool passwordVisibility;
   final scaffoldKey = GlobalKey<ScaffoldState>();
+  final formGlobalKey = GlobalKey<FormState>();
 
   @override
   void initState() {
@@ -34,161 +35,185 @@ class _LoginWidgetWidgetState extends State<LoginWidget> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsetsDirectional.fromSTEB(10, 0, 10, 0),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Padding(
-                padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 10),
-                child: Image.asset(
-                  'assets/images/Hongkong-pana.png',
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height * 0.3,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              const Align(
-                alignment: AlignmentDirectional(-0.9, 0),
-                child: Padding(
-                  padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 5),
-                  child: Text(
-                    'เข้าสู่ระบบ',
-                    textAlign: TextAlign.start,
-                    style: TextStyle(
-                      fontSize: 28,
+          child: Form(
+              key: formGlobalKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Padding(
+                    padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 10),
+                    child: Image.asset(
+                      'assets/images/Hongkong-pana.png',
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height * 0.3,
+                      fit: BoxFit.cover,
                     ),
                   ),
-                ),
-              ),
-              Align(
-                alignment: const AlignmentDirectional(0, 0),
-                child: Padding(
-                  padding: const EdgeInsetsDirectional.fromSTEB(0, 15, 0, 0),
-                  child: TextFormField(
-                    controller: telController,
-                    obscureText: false,
-                    decoration: InputDecoration(
-                      labelText: 'เบอร์โทรศัพท์',
-                      labelStyle: const TextStyle(
-                        fontFamily: 'Bai Jamjuree',
-                        fontWeight: FontWeight.w300,
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: const BorderSide(
-                          color: Color(0xFFA7A5A5),
-                          width: 1,
+                  const Align(
+                    alignment: AlignmentDirectional(-0.9, 0),
+                    child: Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 5),
+                      child: Text(
+                        'เข้าสู่ระบบ',
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                          fontSize: 28,
                         ),
-                        borderRadius: BorderRadius.circular(10),
                       ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: const BorderSide(
-                          color: Color(0xFFA7A5A5),
-                          width: 1,
+                    ),
+                  ),
+                  Align(
+                    alignment: const AlignmentDirectional(0, 0),
+                    child: Padding(
+                      padding:
+                          const EdgeInsetsDirectional.fromSTEB(0, 15, 0, 0),
+                      child: TextFormField(
+                        controller: telController,
+                        obscureText: false,
+                        decoration: InputDecoration(
+                          labelText: 'เบอร์โทรศัพท์',
+                          labelStyle: const TextStyle(
+                            fontFamily: 'Bai Jamjuree',
+                            fontWeight: FontWeight.w300,
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0xFFA7A5A5),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0xFFA7A5A5),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
                         ),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                    style: const TextStyle(
-                      fontFamily: 'Bai Jamjuree',
-                      fontWeight: FontWeight.w300,
-                    ),
-                    textAlign: TextAlign.start,
-                    keyboardType: TextInputType.phone,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsetsDirectional.fromSTEB(0, 15, 0, 0),
-                child: TextFormField(
-                  controller: passwordController,
-                  obscureText: !passwordVisibility,
-                  decoration: InputDecoration(
-                    labelText: 'รหัสผ่าน',
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(
-                        color: Color(0xFFA7A5A5),
-                        width: 1,
-                      ),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(
-                        color: Color(0xFFA7A5A5),
-                        width: 1,
-                      ),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    suffixIcon: InkWell(
-                      onTap: () => setState(
-                        () => passwordVisibility = !passwordVisibility,
-                      ),
-                      child: Icon(
-                        passwordVisibility
-                            ? Icons.visibility_outlined
-                            : Icons.visibility_off_outlined,
-                        color: const Color(0xFF757575),
-                        size: 22,
-                      ),
-                    ),
-                  ),
-                  keyboardType: TextInputType.visiblePassword,
-                ),
-              ),
-              Padding(
-                  padding: const EdgeInsetsDirectional.fromSTEB(0, 20, 0, 5),
-                  child: SizedBox(
-                    width: 350,
-                    height: 50,
-                    child: ElevatedButton(
-                        onPressed: () {
-                          // Navigator.push(
-                          //     context,
-                          //     MaterialPageRoute(
-                          //         builder: (context) =>
-                          //             const ColorTestWidget()));
+                        style: const TextStyle(
+                          fontFamily: 'Bai Jamjuree',
+                          fontWeight: FontWeight.w300,
+                        ),
+                        textAlign: TextAlign.start,
+                        keyboardType: TextInputType.phone,
+                        validator: (val) {
+                          if (val.isEmpty) {
+                            return 'Phone number is required';
+                          }
+                          if (val.length != 10) {
+                            return 'Phone number requires 10 numbers.';
+                          }
+                          return null;
                         },
-                        child: const Text('เข้าสู่ระบบ'),
-                        style: ElevatedButton.styleFrom(
-                            shape: const StadiumBorder(),
-                            primary: Colors.deepPurpleAccent,
-                            textStyle: const TextStyle(
-                                fontSize: 18, fontFamily: 'BaiJamjuree'))),
-                  )),
-              Padding(
-                  padding: const EdgeInsetsDirectional.fromSTEB(0, 5, 0, 5),
-                  child: SizedBox(
-                      width: 350,
-                      height: 50,
-                      child: OutlinedButton(
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      const RegisterWidget()));
-                        },
-                        child: const Text(
-                          'สมัครสมาชิก',
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsetsDirectional.fromSTEB(0, 15, 0, 0),
+                    child: TextFormField(
+                      controller: passwordController,
+                      obscureText: !passwordVisibility,
+                      decoration: InputDecoration(
+                        labelText: 'รหัสผ่าน',
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                            color: Color(0xFFA7A5A5),
+                            width: 1,
+                          ),
+                          borderRadius: BorderRadius.circular(10),
                         ),
-                        style: ElevatedButton.styleFrom(
-                            shape: const StadiumBorder(),
-                            textStyle: const TextStyle(
-                                fontSize: 18,
-                                fontFamily: 'BaiJamjuree',
-                                color: Colors.deepPurpleAccent)),
-                      ))),
-              TextButton(
-                  onPressed: () {
-                    print('Forget Password Button');
-                  },
-                  child: const Text('ลืมรหัสผ่าน?'),
-                  style: TextButton.styleFrom(
-                      shape: const StadiumBorder(),
-                      textStyle: const TextStyle(
-                          fontSize: 14,
-                          fontFamily: 'BaiJamjuree',
-                          color: Colors.deepPurpleAccent)))
-            ],
-          ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                            color: Color(0xFFA7A5A5),
+                            width: 1,
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        suffixIcon: InkWell(
+                          onTap: () => setState(
+                            () => passwordVisibility = !passwordVisibility,
+                          ),
+                          child: Icon(
+                            passwordVisibility
+                                ? Icons.visibility_outlined
+                                : Icons.visibility_off_outlined,
+                            color: const Color(0xFF757575),
+                            size: 22,
+                          ),
+                        ),
+                      ),
+                      keyboardType: TextInputType.visiblePassword,
+                      validator: (val) {
+                        if (val.length >= 8) {
+                          return null;
+                        } else {
+                          return 'Password is required at least 8 characters';
+                        }
+                      },
+                    ),
+                  ),
+                  Padding(
+                      padding:
+                          const EdgeInsetsDirectional.fromSTEB(0, 20, 0, 5),
+                      child: SizedBox(
+                        width: 350,
+                        height: 50,
+                        child: ElevatedButton(
+                            onPressed: () {
+                              if (formGlobalKey.currentState.validate()) {
+                                formGlobalKey.currentState.save();
+                                print(passwordController.text);
+                                // Navigator.push(
+                                //     context,
+                                //     MaterialPageRoute(
+                                //         builder: (context) =>
+                                //             const RegisterWidget()));
+                              }
+                            },
+                            child: const Text('เข้าสู่ระบบ'),
+                            style: ElevatedButton.styleFrom(
+                                shape: const StadiumBorder(),
+                                primary: Colors.deepPurpleAccent,
+                                textStyle: const TextStyle(
+                                    fontSize: 18, fontFamily: 'BaiJamjuree'))),
+                      )),
+                  Padding(
+                      padding: const EdgeInsetsDirectional.fromSTEB(0, 5, 0, 5),
+                      child: SizedBox(
+                          width: 350,
+                          height: 50,
+                          child: OutlinedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const RegisterWidget()));
+                            },
+                            child: const Text(
+                              'สมัครสมาชิก',
+                            ),
+                            style: ElevatedButton.styleFrom(
+                                shape: const StadiumBorder(),
+                                textStyle: const TextStyle(
+                                    fontSize: 18,
+                                    fontFamily: 'BaiJamjuree',
+                                    color: Colors.deepPurpleAccent)),
+                          ))),
+                  TextButton(
+                      onPressed: () {
+                        print('Login as guest');
+                      },
+                      child: const Text('เข้าสู่ระบบแบบ guest'),
+                      style: TextButton.styleFrom(
+                          shape: const StadiumBorder(),
+                          textStyle: const TextStyle(
+                              fontSize: 14,
+                              fontFamily: 'BaiJamjuree',
+                              color: Colors.deepPurpleAccent)))
+                ],
+              )),
         ),
       ),
     );
