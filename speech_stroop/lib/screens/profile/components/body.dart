7540d409bb9582d246a48337caf0c7383c65fff5
Justproy <t.suthada@gmail.com>
2022-03-17 04:04:51 +0700

    feat(screen): init profile

---
+++ speech_stroop/lib/screens/profile/components/body.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:speech_stroop/components/button/primary_button.dart';
+import 'package:speech_stroop/components/button/secondary_button.dart';
+import 'package:speech_stroop/screens/auth/login.dart';
+
+class Body extends StatefulWidget {
+  const Body({Key key}) : super(key: key);
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('ข้อมูลส่วนตัว'),
+        ),
+        body: Center(
+            child: Column(
+          children: [
+            PrimaryButton(
+                "แก้ไขข้อมูล",
+                () => {
+                      showSimpleModalDialog(context),
+                    }),
+            TextButton(
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+              ),
+              onPressed: () {
+                print('log out');
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const LoginScreen()));
+              },
+              child: const Text('ออกจากระบบ'),
+            )
+          ],
+        )));
+  }
+
+  showSimpleModalDialog(context) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return Dialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)),
+            child: Container(
+              constraints: const BoxConstraints(maxHeight: 270),
+              child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      alignment: Alignment.topRight,
+                      child: IconButton(
+                        onPressed: () => Navigator.pop(context, false),
+                        icon: const Icon(
+                          Icons.close,
+                          color: Color(0xFF37265F),
+                        ),
+                        iconSize: 30,
+                      ),
+                    ),
+                    Container(
+                        alignment: Alignment.center,
+                        child: (const Text('ต้องการบันทึกหรือไม่',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 16,
+                                color: Color(0xFF22005D),
+                                fontFamily: 'BaiJamjuree',
+                                wordSpacing: 1)))),
+                    Container(
+                      padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                      child: Column(children: [
+                        PrimaryButton("บันทึก", () => {print('save')}),
+                        SecondaryButton("ยกเลิก", () => {print('cancle')})
+                      ]),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        });
+  }
+}
