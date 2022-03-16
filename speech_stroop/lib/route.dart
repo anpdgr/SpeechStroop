ad2a2c296c7f00357392a3c795ced60015952558
Justproy <t.suthada@gmail.com>
2022-03-16 22:16:33 +0700

    feat(route): add screens route

--- speech_stroop/lib/route.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-// All our routes will be available here
-final Map<String, WidgetBuilder> routes = {
-  //SplashScreen.routeName: (context) => SplashScreen(),
-};
