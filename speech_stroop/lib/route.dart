fc42798faf81fb3a59020b825b4df360187500e4
Justproy <t.suthada@gmail.com>
2022-03-16 20:27:26 +0700

    feat(lib): init shared files

---
+++ speech_stroop/lib/route.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/widgets.dart';
+
+// All our routes will be available here
+final Map<String, WidgetBuilder> routes = {
+  //SplashScreen.routeName: (context) => SplashScreen(),
+};
---
+++ speech_stroop/lib/theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'constants.dart';
+
+ThemeData theme() {
+  return ThemeData();
+}
+
+InputDecorationTheme inputDecorationTheme() {
+  OutlineInputBorder outlineInputBorder = OutlineInputBorder();
+  return InputDecorationTheme();
+}
+
+TextTheme textTheme() {
+  return TextTheme(
+    bodyText1: TextStyle(),
+    bodyText2: TextStyle(),
+  );
+}
+
+AppBarTheme appBarTheme() {
+  return AppBarTheme();
+}
