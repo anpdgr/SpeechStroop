77444f1d688a610c7bc97ef1dd9c1efce7104cbf
Justproy <t.suthada@gmail.com>
2022-03-27 04:06:30 +0700

    feat(profile): call profile api

--- speech_stroop/lib/screens/profile/profile_screen.dart
+++ speech_stroop/lib/screens/profile/profile_screen.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:speech_stroop/components/coustom_bottom_nav_bar.dart';
 import 'package:speech_stroop/components/home_screen_appbar.dart';
 import 'package:speech_stroop/enums.dart';
+import 'package:speech_stroop/model/auth.dart';
+import 'package:speech_stroop/model/user.dart';
+import 'package:speech_stroop/screens/auth/login.dart';
 import 'package:speech_stroop/screens/profile/components/body.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 Map registerReq = {};
-var userProfile;
+User userProfile;
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key key}) : super(key: key);
@@ -24,15 +27,20 @@ class ProfileScreen extends StatelessWidget {
 }
 
 getUserProfile() async {
-  var res = await http.get(
-    Uri.parse("http://localhost:3000/user/profile"),
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization':
-          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjNlYjU0ZjUxODg0NTIwYTRkZThlOTUiLCJpYXQiOjE2NDgyNzY4MjZ9.MGGqHkHGBBQps-wzrtTPTHv4lRY4yLatgUE5QNELdLE',
-    },
-  );
-  print(res.body);
-  print("res type:" + res.body.runtimeType.toString());
-  return res.body;
+  if (userProfile == null) {
+    String token = auth.token;
+    var res = await http.get(
+      Uri.parse("http://localhost:3000/user/profile"),
+      headers: {
+        'Authorization': 'Bearer $token',
+      },
+    );
+    if (res.statusCode == 200) {
+      userProfile = User.fromJson(jsonDecode(res.body));
+    } else {
+      //TODO: handle
+    }
+    print(userProfile);
+  }
+  return userProfile;
 }
