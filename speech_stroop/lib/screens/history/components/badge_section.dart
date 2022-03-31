8a2e96c0f62da23926fe6ebb43524431c8092a75
Justproy <t.suthada@gmail.com>
2022-04-01 04:23:30 +0700

    refactor(history): resize component

---
+++ speech_stroop/lib/screens/history/components/badge_section.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:speech_stroop/constants.dart';
+import 'package:speech_stroop/model/test_module/history.dart';
+import 'package:speech_stroop/screens/history/components/badge_box.dart';
+
+class BadgeSection extends StatelessWidget {
+  const BadgeSection(this.scoreHistory, {Key key}) : super(key: key);
+  final List<History> scoreHistory;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 10, bottom: 10),
+      padding: const EdgeInsets.all(30.0),
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "รางวัลที่ได้รับ",
+              textAlign: TextAlign.start,
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            BadgeBox(scoreHistory, "ทดสอบต่อเนื่อง"),
+            const SizedBox(height: 20),
+            BadgeBox(scoreHistory, "คะแนนยอดเยี่ยม"),
+          ]),
+    );
+  }
+}
--- speech_stroop/lib/screens/history/components/body.dart
+++ speech_stroop/lib/screens/history/components/body.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:speech_stroop/components/button/primary_button.dart';
 import 'package:speech_stroop/constants.dart';
 import 'package:speech_stroop/model/test_module/history.dart';
+import 'package:speech_stroop/screens/history/components/badge_section.dart';
+import 'package:speech_stroop/screens/history/components/score_bar_section.dart';
 import 'package:speech_stroop/screens/history_all/history_all_screen.dart';
 
 class Body extends StatefulWidget {
@@ -35,117 +37,8 @@ class _BodyState extends State<Body> {
                         await getHistory(),
                         Navigator.pushNamed(context, HistoryAllScreen.routeName)
                       }),
-              Container(
-                margin: EdgeInsets.all(40),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(
-                        "คะแนนล่าสุด",
-                        textAlign: TextAlign.left,
-                        style: Theme.of(context).textTheme.titleMedium,
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                            color: softPrimaryColor,
-                            borderRadius: BorderRadius.circular(10)),
-                        padding: EdgeInsets.all(40),
-                        child: Column(children: [
-                          Text(
-                            "คะแนนเฉลี่ยต่อสัปดาห์",
-                            textAlign: TextAlign.left,
-                            style: Theme.of(context)
-                                .textTheme
-                                .labelLarge
-                                .apply(color: Color(0xFF525252)),
-                          ),
-                          Text(
-                            "27",
-                            textAlign: TextAlign.left,
-                            style: Theme.of(context).textTheme.headlineMedium,
-                          ),
-                        ]),
-                      )
-                    ]),
-              ),
-              Container(
-                margin: EdgeInsets.all(40),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(
-                        "รางวัลที่ได้รับ",
-                        textAlign: TextAlign.left,
-                        style: Theme.of(context).textTheme.titleMedium,
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                            color: softPrimaryColor,
-                            borderRadius: BorderRadius.circular(10)),
-                        padding: EdgeInsets.all(40),
-                        child: Column(children: [
-                          Row(
-                            children: [
-                              Text(
-                                "คะแนนยอดเยี่ยม",
-                                textAlign: TextAlign.left,
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .titleMedium
-                                    .apply(color: Color(0xFF464D59)),
-                              ),
-                              Text("0/6",
-                                  textAlign: TextAlign.right,
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .labelLarge
-                                      .apply(color: secondaryColor)),
-                            ],
-                          ),
-                          const Divider(
-                            color: Color(0xFF381E73),
-                            height: 25,
-                            thickness: 2,
-                            indent: 5,
-                            endIndent: 5,
-                          ),
-                        ]),
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                            color: softPrimaryColor,
-                            borderRadius: BorderRadius.circular(10)),
-                        padding: EdgeInsets.all(40),
-                        child: Column(children: [
-                          Row(
-                            children: [
-                              Text(
-                                "ทดสอบต่อเนื่อง",
-                                textAlign: TextAlign.left,
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .titleMedium
-                                    .apply(color: Color(0xFF464D59)),
-                              ),
-                              Text("0/6",
-                                  textAlign: TextAlign.right,
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .labelLarge
-                                      .apply(color: secondaryColor)),
-                            ],
-                          ),
-                          const Divider(
-                            color: Color(0xFF381E73),
-                            height: 25,
-                            thickness: 2,
-                            indent: 5,
-                            endIndent: 5,
-                          ),
-                        ]),
-                      )
-                    ]),
-              )
+              ScoreBarSection(userHistory),
+              BadgeSection(userHistory),
             ],
           ))
     ])));
