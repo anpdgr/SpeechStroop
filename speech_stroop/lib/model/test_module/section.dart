768ddd3db5a10ae594bd1bd58fca76ce08c3f04c
Justproy <t.suthada@gmail.com>
2022-04-01 02:33:16 +0700

    fix(model): fix json decode and add fromJson

--- speech_stroop/lib/model/test_module/section.dart
+++ speech_stroop/lib/model/test_module/section.dart
@@ -7,8 +7,28 @@ class Section {
   List<Question> questions;
   String audioUrl;
 
-  Section(this.section, this.score, this.avgReactionTimeMs, this.questions,
-      this.audioUrl);
+  Section(
+      [this.section,
+      this.score,
+      this.avgReactionTimeMs,
+      this.questions,
+      this.audioUrl]);
+
+  factory Section.fromJson(dynamic json) {
+    dynamic avgReactionTimeMs = json['avgReactionTimeMs'] ?? 0;
+    List<Question> questions = json['questions'] != null
+        ? List<Question>.from(
+            json['questions'].map((data) => Question.fromJson(data)))
+        : [];
+
+    return Section(
+      json['section'] as int,
+      Map<String, dynamic>.from(json['score']),
+      avgReactionTimeMs.toDouble() as double,
+      questions,
+      json['audioUrl'] as String,
+    );
+  }
 
   Map<String, dynamic> toJson() {
     return {
