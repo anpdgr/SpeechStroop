8cf0e3877f06a208ba1a3fbacc19e0cf18e8bd42
anpdgr <ant-dgr@hotmail.com>
2022-04-26 22:54:07 +0700

    feat(backend): add firebase storage and upload audio file

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -5,4 +5,4 @@ export const MONGO_URI = process.env.MONGO_URI
 export const PORT = process.env.PORT
 
 export const JWT_SECRET = process.env.JWT_SECRET
-export const SERVICE_ACCOUNT_KEY_PATH = 'credentials.json'
+export const SERVICE_ACCOUNT_KEY_PATH = 'serviceAccountKey.json'
---
+++ backend/src/firebase/upload_storage.ts
@@ -0,0 +1,38 @@
+import { Storage } from '@google-cloud/storage'
+import { SERVICE_ACCOUNT_KEY_PATH } from '../config'
+
+const storage = new Storage({
+  keyFilename: SERVICE_ACCOUNT_KEY_PATH,
+})
+
+let bucketName = 'speech-stroop.appspot.com'
+const audioFolder = 'audio'
+
+// Uploads a local file to the bucket
+export const uploadFile = async ([userId, filePath]: string) => {
+  const fileName = filePath.split('/').pop()
+  const dstPath = `${audioFolder}/${userId}/${fileName}`
+  await storage.bucket(bucketName).upload(filePath, {
+    gzip: true,
+    metadata: {
+      cacheControl: 'public, max-age=31536000',
+    },
+    destination: dstPath,
+  })
+
+  console.log(`${filePath} was uploaded successfully.`)
+
+  const fileUrl = await getFileUrl(dstPath)
+  return fileUrl
+}
+
+const getFileUrl = async (filePath: string) => {
+  const today = new Date()
+  const thisYear = today.getFullYear()
+  const urls = await storage
+    .bucket(bucketName)
+    .file(filePath)
+    .getSignedUrl({ action: 'read', expires: today.setFullYear(thisYear + 5) })
+  const signedUrl = urls[0]
+  return signedUrl
+}
