6d7e322af9fca52658057a869f85f4ab75323b60
Justproy <t.suthada@gmail.com>
2022-04-29 23:35:51 +0700

    feat(user): ch api method

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -10,7 +10,7 @@ router.get('/profile', async (req: RequestWithUser, res) => {
   res.json(req.user)
 })
 
-router.post('/profile', async (req: RequestWithUser, res) => {
+router.patch('/profile', async (req: RequestWithUser, res) => {
   const body = req.body
   const user = await updateUser(body, req.user.id)
   res.json(user)
