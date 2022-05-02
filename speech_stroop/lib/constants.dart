60803b8b0725d9bc87e8b89b37eb3ccfc559997e
Justproy <t.suthada@gmail.com>
2022-05-02 18:41:26 +0700

    feat(const): add stroop const

--- speech_stroop/lib/constants.dart
+++ speech_stroop/lib/constants.dart
@@ -72,8 +72,10 @@ int tutorialQuestionsAmount = 5;
 
 // Stroop
 int stroopQuestionsAmount = 20;
-int stroopQuestionDurationMs = 3; //3000;
-int stroopIntervalDurationMs = 3; //3000;
+int stroopSectionAmount = 3;
+int stroopTotalQuestionsAmount = stroopQuestionsAmount * stroopSectionAmount;
+int stroopQuestionDurationMs = 3000; //3000;
+int stroopIntervalDurationMs = 3000; //3000;
 Map<String, Color> stroopColorsMap = {
   'แดง': const Color(0xFFDA5C54),
   'ดำ': const Color(0xFF000000),
