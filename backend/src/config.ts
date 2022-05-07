a69f5092eb7d762c32f567424306f7d7dea16da0
anpdgr <ant-dgr@hotmail.com>
2022-05-08 00:46:40 +0700

    deploy: set root dir

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const MONGO_URI = process.env.MONGO_URI
-export const PORT = process.env.PORT
+export const PORT = process.env.PORT || 3000
 
 export const JWT_SECRET = process.env.JWT_SECRET
 export const SERVICE_ACCOUNT_KEY_PATH = 'serviceAccountKey.json'
