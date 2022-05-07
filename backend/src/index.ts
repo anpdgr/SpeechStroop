460cdea93f52510f77730057916308f2b149c6e5
anpdgr <ant-dgr@hotmail.com>
2022-05-08 01:41:01 +0700

    deploy: edit port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import cors from 'cors'
 import express from 'express'
 import mongoose from 'mongoose'
-import { MONGO_URI, PORT } from './config'
+import { MONGO_URI } from './config'
 import { errorHandler } from './errors'
 import uploadAudioRouter from './routes/audio'
 import authRouter from './routes/auth'
@@ -35,8 +35,13 @@ console.log(`Connecting to MongoDB...`)
 mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => {
-    app.listen(PORT, () =>
-      console.log(`Connected! App running on port ${PORT}`),
+    app.listen(process.env.PORT || 3000, () =>
+      // console.log(`Connected! App running on port ${PORT}`),
+      console.log(
+        'Express server listening on port %d in %s mode',
+        process.env.PORT,
+        app.settings.env,
+      ),
     )
   })
   .catch((err) => {
